5a98c570a4fd046bc1e699b43d0d76188a962d84
seema1611 <seemarajpure16@gmail.com>
2020-07-06 16:09:41 +0700

    REFACTORED: Refactor the value of unit

--- src/test/java/com/quantitymeasurement/service/QuantityServiceTest.java
+++ src/test/java/com/quantitymeasurement/service/QuantityServiceTest.java
@@ -79,8 +79,8 @@ public class QuantityServiceTest {
     @Test
     void givenQuantityMeasurementInLength_When50CentimeterConvertToInch_ShouldReturn20Inch() {
         QuantityMeasurementService quantityMeasurementService = new QuantityMeasurementService();
-        ConvertDTO convertDTO = new ConvertDTO(1.0,SubQuantities.CENTIMETER,SubQuantities.INCH);
+        ConvertDTO convertDTO = new ConvertDTO(50.0,SubQuantities.CENTIMETER,SubQuantities.INCH);
         double convertedResult = quantityMeasurementService.getConvertedValueOfUnit(convertDTO);
-        Assert.assertEquals(36.01,convertedResult,0.0);
+        Assert.assertEquals(20.0,convertedResult,0.0);
     }
 }
